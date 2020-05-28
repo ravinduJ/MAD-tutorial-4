4193ad080a0ae6b09ce1ff9ca1912517ee72d511
Ravindu Jayakody <48220639+ravinduJ@users.noreply.github.com>
2020-05-28 09:53:11 +0530

    Tutorial 4 complete.

---
+++ app/src/androidTest/java/DataBase/DBHandler.java
@@ -0,0 +1,24 @@
+package DataBase;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHandler extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "UserInfo.db";
+    public DBHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
